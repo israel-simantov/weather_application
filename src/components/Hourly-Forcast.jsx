081f92c47225bda050e816f17b04b84224a8864e
Israel Siman-Tov <your-email@example.com>
2023-07-12 15:13:13 +0300

    first commit

--- src/components/Hourly-Forcast.jsx
+++ src/components/Hourly-Forcast.jsx
@@ -7,7 +7,7 @@ function HourlyForcast() {
     <div>
       <div className='flex box-border mx-auto h-36 w-11/12 bg-sky-900 bg-opacity-20 rounded-2xl'>
       <span className='flex pl-3 pt-2 text-xs  text-white text-opacity-75'><LuClock9 className='mt-px'/><p className='pl-1'>HOURLY FORCAST</p></span>
-        
+        <h1>Hello</h1>
 
 
 
